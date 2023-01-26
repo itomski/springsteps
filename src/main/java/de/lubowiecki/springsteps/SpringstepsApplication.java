799219efda8b8a6aff31cbe98847c387e7a6a592
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2023-01-26 12:29:13 +0100

    Initial commit

---
+++ src/main/java/de/lubowiecki/springsteps/SpringstepsApplication.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.springsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringstepsApplication.class, args);
+    }
+
+}
