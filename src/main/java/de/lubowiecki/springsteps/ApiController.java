c54f724b486d71e58a4a79b78b5ea29c8e6aa316
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2023-01-26 15:13:02 +0100

    Update

---
+++ src/main/java/de/lubowiecki/springsteps/ApiController.java
@@ -0,0 +1,28 @@
+package de.lubowiecki.springsteps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class ApiController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return service.findAll();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getOneProduct(@PathVariable int id) {
+        return service.findById(id).orElse(new Product());
+    }
+}
--- src/main/java/de/lubowiecki/springsteps/WebUIController.java
+++ src/main/java/de/lubowiecki/springsteps/WebUIController.java
@@ -1,5 +1,6 @@
 package de.lubowiecki.springsteps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebUIController {
 
+    @Autowired // CDI - Context and Dependency Injection
+    private ProductService service;
+
     @GetMapping("/")
     public String index(Model model) {
         // model.addAttribute = Weitergabe von Daten an die View
         model.addAttribute("headline", "Herzlich Willkommen bei Spring");
         model.addAttribute("ac", "home");
         model.addAttribute("content", "Das ist das Haus von Nikigraus...");
-        return "hallo"; // Verweist auf /WEB-INF/hallo.jsp (siehe application.properties)
+        return "standard"; // Verweist auf /WEB-INF/standard.jsp (siehe application.properties)
     }
 
     @GetMapping("/team")
@@ -21,6 +25,17 @@ public class WebUIController {
         model.addAttribute("headline", "Unser Team");
         model.addAttribute("ac", "team");
         model.addAttribute("content", "Das sind alle unsere Team-Mitglieder...");
-        return "hallo";
+        return "standard";
     }
+
+    @GetMapping("/products")
+    public String products(Model model) {
+        model.addAttribute("headline", "Unsere Produkte");
+        model.addAttribute("ac", "products");
+        model.addAttribute("productList", service.findAll());
+        return "products";
+    }
+
+    // Schreibe Ein Formular zu Hinzufügen von Produkten
+    // Verwende das Formular in einer Methode des WebUIControllers, so dass das Form aufrufbar ist
 }
