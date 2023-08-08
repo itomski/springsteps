0b8099977ad41727dedc13ce542d49ec057fce04
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2023-08-08 14:13:06 +0200

    Update

--- src/main/java/de/lubowiecki/springsteps/MainController.java
+++ src/main/java/de/lubowiecki/springsteps/MainController.java
@@ -6,11 +6,46 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class MainController {
+
+    /* HTTP-Methoden:
+    GET (@GetMapping): Liefert den Inhalt einer gewünschten Ressource (HTML, JSON, PDF, etc.)
+    POST (@PostMapping): Wird zum Verschicken von Formulardaten benutzt. Kann neue Ressourcen anlegen
+
+    Ein Webbrowser verwendet nur Get und Post
+
+    Für APIs nutzt man auch weitere wie z.B.
+    PUT (@PutMapping): Wird zum verändern von Ressourcen benutzt
+    DELETE (@DeleteMapping): Löscht eine Ressource
+     */
+
+    /* Aktuell erreichbare URL-Mappings
+    | METHODE   | URL           | Controller        | Methode       |
+    | GET       | /             | MainController    | index         |
+    | GET       | /products     | MainController    | products      |
+    | GET       | /new          | MainController    | form          |
+    | POST      | /save         | MainController    | save          |
+    | GET       | /edit/1       | MainController    | edit          |
+    | GET       | /delete/1     | MainController    | delete        |
+     */
+
+    /*
+        Model (org.springframework.ui.Model) ist ein Speicher, mit dem man Variablen
+        an Thymeleaf-HTML-Templates transportieren kann
+        model.addAttribute("schluessel", "wert");
+        Schlüssel muss immer ein String sein
+        Wert kann ein Objekt, Collection, Array oder auch primitiver Wert sein
+     */
+
+    /*
+        Model wird als Transportschicht zwischen Controller und HTML-Templates verwendet
+        Das gefüllte Model-Objekt wird automatisch an das HTML-Template weitergegeben,
+        das im return der Methode steht
+     */
+
 
     @Autowired // Dieses Objekt wird automatisch vom Spring-Container instanziert und zugewiesen
     private ProductRepository repository;
@@ -26,8 +61,10 @@ public class MainController {
 
     // http://localhost:8080/products
     @GetMapping("/products")
-    public String getProducts(Model model) {
+    public String products(Model model) {
         model.addAttribute("headline", "Unsere Produkte");
+
+        // findAll: Liefert eine Liste aller Produkte
         model.addAttribute("products", repository.findAll());
         model.addAttribute("cur", "products");
         return "list"; // Wir verwenden list.html
@@ -42,19 +79,14 @@ public class MainController {
         return "form";
     }
 
-    /* HTTP-Methoden:
-    GET
-    POST
-    PUT
-    DELETE
-    ...
-     */
-
     // Post: http://localhost:8080/save
     @PostMapping("/save")
-    public String save(Product product) {
+    public String save(Product product) { // Formulardaten werden zu einem Produkt-Objekt zusammengesetzt
+        // save: Speichert ein Objekt in der DB
+        // ist die id = 0 wird ein neues Objekt erzeugt
+        // ist es != 0 und ein passendes Objekt ist in der DB, wird es geändert
         repository.save(product);
-        return "redirect:/products";
+        return "redirect:/products"; // Umleitung auf eine andere URL
     }
 
     // http://localhost:8080/edit/10
@@ -62,6 +94,10 @@ public class MainController {
     public String edit(@PathVariable int id, Model model) {
         model.addAttribute("headline", "Produkt bearbeiten");
         model.addAttribute("cur", "products");
+
+        // findById: sucht ein Produkt in der DB. Liefert das Produkt verpackt in ein Optional
+        // Gibt es für die ID nichts passendes in der DB ist das Optional empty
+
         Product product = repository.findById(id).orElse(new Product()); // Entweder das gesuchte Produkt oder wenn nicht vorhanden ein leeres
         model.addAttribute("product", product);
         return "form";
@@ -71,6 +107,7 @@ public class MainController {
     // @PathVariable liest den Wert für den Parameter aus der URL
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable int id) {
+        // deleteById: löscht ein Produkt aus der DB, wenn die ID verfügbar ist
         repository.deleteById(id);
         return "redirect:/products";
     }
